0b5cd6536f3e579a15b69ce9d818dd7d6f15f8ae
unknown <Administrator@cse-rd11.SAEC.EDU>
2021-10-09 10:20:57 +0530

    added gitignore, getting user input
    added gitignore for the project
    
    implemented the feature for getting user input in employee console
    application

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string id,name,mobile,dob,doj,sex,salary;
+            Console.WriteLine("Welcome to the Employee application, Please enter the following details\n");
+            Console.WriteLine("enter the employee id");
+            id = Console.ReadLine();
+            Console.WriteLine("enter the employee name");
+            name = Console.ReadLine();
+            Console.WriteLine("enter the employee mobile");
+            mobile = Console.ReadLine();
+            Console.WriteLine("enter the employee DOB");
+            dob = Console.ReadLine();
+            Console.WriteLine("enter the employee DOJ");
+            doj = Console.ReadLine();
+            Console.WriteLine("enter the employee sex");
+            sex = Console.ReadLine();
+            Console.WriteLine("enter the employee salary");
+            salary = Console.ReadLine();
+        }
+    }
+}
